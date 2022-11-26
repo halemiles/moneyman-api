949f6f392dcff214c3f68eb39702aaa71553d242
Miles Hale <hale.miles@gmail.com>
2022-11-26 23:30:38 +0000

    refactor: Removed some moneyman context constructors

--- src/Moneyman.Domain/MoneymanContext.cs
+++ src/Moneyman.Domain/MoneymanContext.cs
@@ -13,15 +13,6 @@ namespace Moneyman.Domain
         private bool IsTest { get; set; } 
         public MoneymanContext(DbContextOptions<MoneymanContext> options) : base(options)
         {
-            IsTest = true;
-        }
-        public MoneymanContext(DbContextOptions<MoneymanContext> options, IConfiguration configuration) : base(options)
-        {
-            Configuration = configuration;
-        }
-        public MoneymanContext(IConfiguration configuration)
-        {
-            Configuration = configuration;
         }
 
         public MoneymanContext(){}
@@ -31,10 +22,6 @@ namespace Moneyman.Domain
        
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            if(!IsTest)
-            {
-                options.UseSqlite(new SqliteConnection(Configuration.GetConnectionString("WebApiDatabase")));            
-            }
             
         }
 
