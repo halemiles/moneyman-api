5c415410ee1e223b97dfa7466fed3c9ee77803fb
Miles <hale.miles@gmail.com>
2022-11-27 00:32:43 +0000

    Tests/261122 (#45)
    * refactor: Removed some moneyman context constructors
    
    * refactor: further context tidyup
    
    * test: added mapper tests

--- src/Moneyman.Domain/MoneymanContext.cs
+++ src/Moneyman.Domain/MoneymanContext.cs
@@ -8,35 +8,14 @@ namespace Moneyman.Domain
 {
     [ExcludeFromCodeCoverage]
     public class MoneymanContext : DbContext
-    {        
-        protected readonly IConfiguration Configuration;
-        private bool IsTest { get; set; } 
-        public MoneymanContext(DbContextOptions<MoneymanContext> options) : base(options)
-        {
-            IsTest = true;
-        }
-        public MoneymanContext(DbContextOptions<MoneymanContext> options, IConfiguration configuration) : base(options)
-        {
-            Configuration = configuration;
-        }
-        public MoneymanContext(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+    {
+        public MoneymanContext(DbContextOptions<MoneymanContext> options) : base(options) {}
         public MoneymanContext(){}
         public DbSet<Transaction> Transactions {get; set;}
         public DbSet<Payday> Paydays {get; set;}
         public DbSet<PlanDate> PlanDates {get; set;}
        
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            if(!IsTest)
-            {
-                options.UseSqlite(new SqliteConnection(Configuration.GetConnectionString("WebApiDatabase")));            
-            }
-            
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {}
 
     }
 }
