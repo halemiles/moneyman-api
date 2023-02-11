6e020100e462dccb1593b436c69c757a83e63ffa
Miles Hale <hale.miles@gmail.com>
2023-02-11 17:04:28 +0000

    feat: Updated transaction to include IsAnticipated field

--- src/Moneyman.Domain/Transaction.cs
+++ src/Moneyman.Domain/Transaction.cs
@@ -5,6 +5,7 @@ namespace Moneyman.Domain
     public class Transaction : BaseTransaction
     {
         public Frequency Frequency { get; set; }
+        public bool IsAnticipated {get; set;}
         
         //public PaymentType PaymentType { get; set; }
         //public CategoryType CategoryType {get; set;}
