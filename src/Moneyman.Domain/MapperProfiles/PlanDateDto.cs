24c9f0e7d44ca3902066d8d0635f78fbf3ed0bc4
Miles <mhale@protonmail.com>
2023-11-18 20:39:30 +0000

    Added unit tests for reading plan dates

--- src/Moneyman.Domain/MapperProfiles/PlanDateDto.cs
+++ src/Moneyman.Domain/MapperProfiles/PlanDateDto.cs
@@ -11,7 +11,7 @@ namespace Moneyman.Domain.MapperProfiles
             CreateMap<PlanDate,PlanDateDto>()
             .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Date))
             .ForMember(dest => dest.TransactionName, opt => opt.MapFrom(src => src.Transaction.Name))
-            //.ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Transaction.Amount))
+            .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Transaction.Amount))
             .ReverseMap();
         }
     }
