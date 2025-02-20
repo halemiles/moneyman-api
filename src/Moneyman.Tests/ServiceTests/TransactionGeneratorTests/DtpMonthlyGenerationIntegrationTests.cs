e8ed552d1e423fb14c3388e99d91f69a966464f7
Miles <mhale@protonmail.com>
2025-02-20 19:25:57 +0000

    Remove redundant expectedDayValues initialization in DtpMonthlyGenerationIntegrationTests

--- src/Moneyman.Tests/ServiceTests/TransactionGeneratorTests/DtpMonthlyGenerationIntegrationTests.cs
+++ src/Moneyman.Tests/ServiceTests/TransactionGeneratorTests/DtpMonthlyGenerationIntegrationTests.cs
@@ -119,7 +119,6 @@ namespace Moneyman.Tests
         )
         {
             // Arrange
-            List<int> expectedDayValues = new() {day1,day2,day3,day4,day5,day6,day7,day8,day9,day10,day11,day12};
             var startDate = DateTime.Parse(startDateString);
             var sut = NewDtpService();
 
@@ -151,7 +150,6 @@ namespace Moneyman.Tests
         )
         {
             // Arrange
-            List<int> expectedDayValues = new() {day1,day2,day3,day4,day5,day6,day7,day8,day9,day10,day11,day12};
             var startDate = DateTime.Parse(startDateString);
             var sut = NewDtpService();
 
