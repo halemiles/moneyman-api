f14ac6f82d150b38bd2f61b36bd952298c9dd1d9
Miles <mhale@protonmail.com>
2025-02-20 19:27:46 +0000

    Remove CHANGELOG.md and update transaction test formatting

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -24,16 +24,16 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5000',
+    baseURL: "http://localhost:5000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: { ...devices['Desktop Chrome'] },
     },
 
